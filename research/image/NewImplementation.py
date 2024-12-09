cbc742443da65098e4699bc60538bf84d5706f0b
pi <pi@172.27.108.196>
2024-12-09 07:38:40 -0600

    forgot to change it to make it dynamic

--- research/image/NewImplementation.py
+++ research/image/NewImplementation.py
@@ -16,7 +16,7 @@ path = "/home/pi/CapstoneProjectBackUp/research/image/Data"
 image_import_path = "/home/pi/CapstoneProjectBackUp/research/image/Data/raw_imports"
 crop_height = int(SCREEN_HEIGHT * 0.10)  # This will be 120 pixels
 ifblue = False
-use_live_camera = False  # Set this to False to load image from file
+use_live_camera = True  # Set this to False to load image from file
 image_center = 0 
 
 camera = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
