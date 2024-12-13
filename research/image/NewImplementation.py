6abcb0dd8c35c83609fa1d17caac15893b83998b
B1WAYN3 <112424397+B1WAYN3@users.noreply.github.com>
2024-12-13 16:07:41 -0600

    increasing rectangle size for removing data.

--- research/image/NewImplementation.py
+++ research/image/NewImplementation.py
@@ -101,7 +101,7 @@ for i in times2Run:
     img_crop_hsv = img_hsv
 
     center_rect_width_start = int(img_hsv.shape[1] * 0.4)
-    center_rect_width_end = int(img_hsv.shape[1] * 0.6) + int((img_hsv.shape[1] * 0.6 - img_hsv.shape[1] * 0.4) / 3)
+    center_rect_width_end = int(img_hsv.shape[1] * 0.71)
     center_rect = img_hsv[int(img_hsv.shape[0] * 0.4):int(img_hsv.shape[0] * 0.6), center_rect_width_start:center_rect_width_end]
 
     median_brightness_center = np.median(center_rect[:, :, 2])
